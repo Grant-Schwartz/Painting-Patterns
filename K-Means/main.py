92b9c6bbaafb5ee10716d846250dbb6f392de78e
Grant Schwartz <grantwschwartz@gmail.com>
2020-06-29 09:28:20 -0400

    init

---
+++ K-Means/main.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread("Blue.jpeg")
+
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# reshape the image to a 2D array of pixels and 3 color values (RGB)
+pixel_values = image.reshape((-1, 3))
+# convert to float
+pixel_values = np.float32(pixel_values)
+
+print(pixel_values.shape)
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+
+k = 3
+_, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+centers = np.uint8(centers)
+
+# flatten the labels array
+labels = labels.flatten()
+
+segmented_image = centers[labels.flatten()]
+
+segmented_image = segmented_image.reshape(image.shape)
+# show the image
+plt.imshow(segmented_image)
+plt.show()
